3d82316fa2aea91137231f828fd5f216e14b8f26
Aleksandr <okaliuk1@gmail.com>
2018-12-24 21:01:28 +0200

    create test soutTest for check correct build recompiling.

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -24,6 +24,10 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
+  @Test
+  public void soutTest(){
+    System.out.println("Hello world!");
+  }
 
 
 
